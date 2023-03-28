fb04caaeccc9a5222204df03f92f16afa1a9b293
Romain Komorn <rkomorn@dofc.org>
2023-03-28 10:29:17 +0100

    Move GuessInput handleSubmit to function Other minor tweaks

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,16 +2,21 @@ import React from "react";
 
 const GuessInput = () => {
   const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
   return (
     <form className="guess-input-wrapper"
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log("guess:", guess);
-            setGuess("");
-          }
-          }>
+          onSubmit={handleSubmit}
+    >
       <label htmlFor="guess-input">Enter guess:</label>
-      <input required={true} maxLength="5" minLength="5" id="guess-input" pattern="[A-Za-z]{5}" value={guess}
+      <input id="guess-input" title="5 letter word" required minLength={5} maxLength={5} pattern="[A-Za-z]{5}"
+             type="text"
+             value={guess}
              onChange={event => setGuess(event.target.value.toUpperCase())}></input>
     </form>
   );
